4236ab57ee998b63e201dba03c3fde30fd3594df
Xiaoxiao Wang <shawnwang0123@gmail.com>
2016-03-03 15:29:26 -0700

    Initial Commit

---
+++ CurrencyConverter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CurrencyConverter
+//
+//  Created by Xiaoxiao on 3/3/16.
+//  Copyright © 2016 WangXiaoxiao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
